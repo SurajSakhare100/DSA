5a94c1c0667ea4623cac74fb78537bb7fbffca91
Suraj Sakhare <122685473+SurajSakhare100@users.noreply.github.com>
2023-09-18 16:41:27 +0530

    pattern

--- patterns/lect3.cpp
+++ patterns/lect3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    //dsa course
+    //dsa course - pattern
 /*********** Patterns using while loop ************/
 
 // Q1. print following pattern :
